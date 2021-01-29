b59e83d4e41fd761ba7e8f09652e04920876dc4d
Nomi <naumanahmed19@gmail.com>
2021-01-29 22:33:31 +0100

    enh(game): Added basic game skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <ScoreBoard />
+  <h3>Current Turn : {{players[currentPlayer].name}}</h3>
+  <button @click="changeTurn(1)">Change Turn</button>
+
+  <ul>
+      <li v-for="(player,index) in players" :key="index">
+        {{player.name}} | Score : {{getScore(index)}}
+      </li>
+  </ul>
 </template>
 
 <script>
@@ -9,6 +17,42 @@ export default {
   name: 'App',
   components: {
     ScoreBoard
-  }
+  },
+  data() {
+    return {
+      currentPlayer : 0,
+      players: [
+      {
+  
+        name: 'Ahmad',
+        score: [],
+      },
+      {
+
+        name: 'Ali',
+        score: [],
+      }
+    ],
+    }
+  },
+
+  methods: {
+    changeTurn(p){
+      this.currentPlayer = p; 
+    },
+
+    updateScore(s){
+      this.players[this.currentPlayer].score.push(s)
+    },
+
+    getScore(index){
+      let playerScore = this.players[index].score;
+      return this.calculateScore(playerScore);
+    },
+
+    calculateScore(score){
+      //calculate score here.
+    }
+  },
 }
 </script>
