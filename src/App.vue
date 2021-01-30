004631c874722e8c60da9d935d124d93f6f9b72d
Nomi <naumanahmed19@gmail.com>
2021-01-30 15:31:37 +0100

    fix(game): score calculations

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,12 @@
         {{player.name}} | TotalScore : {{getScore(index)}}
         <table border="1">
           <tr>
-            <td v-for="(frame,index) in player.frames" :key="index" >
-                {{frame}}
+            <td v-for="(n, i) in 10" :key="i" >
+              {{n}}
+               <hr>
+                {{player.frames[i]}}
                 <hr>
-                {{ player.faramScore[index]}}
+                {{ player.faramScore[i]}}
                 <!-- {{calculeFrameScore(player.frames,frame,index)}} -->
                
             </td>
@@ -37,14 +39,16 @@ export default {
       players: [
       {
         name: 'Ahmad',
-       // frames: [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']]
-        frames:[[8,'/'],[9,0],[4,4],[7,2],[9,0],['X'],['X'],[8,0],[3,5],[9,1,7]],
+    //frames: [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
+     frames:[[8,'/'],[9,0],[4,4],[7,2],[9,0],['X'],['X'],[8,0],[3,5],[9,1,7]],
+     //frames: [['X'],['X'],['X'],[5,1],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
         faramScore:[],
       },
       {
 
         name: 'Ali',
-        frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],[8,0],[5,1],[3,'/'],[9,0,0]],
+       frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],[8,0],[5,1],[3,'/'],[9,0,0]],
+        //  frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],['X'],[8,0],[3,'/'],[9,0,0]],
         faramScore:[],
         
       }
@@ -71,30 +75,55 @@ export default {
       frames.forEach((frame,index) => {
         score += this.calculeFrameScore(frames,frame,index);
         console.log('Index:',index, score);
-         faramScore.push(score);
+        faramScore.push(score);
       });
       
       return score;
     },
-
+    hasFrame(index){
+      return typeof frames[index] !== 'undefined'
+    },
     calculeFrameScore(frames,frame,index){
      
       let s = 0;
-      if(index == 10) return s;
-      if(frame.includes( 'X') ){ 
+     // if(index == 10) return s;  //TODO: Check 
+     
+      if(frame.includes( 'X') && index == 8 ){
+
+          s +=frames[index+1].reduce((a,b)=>this.format(a)+this.format(b) , 0) ; 
+
+      }
+     else if(frame.includes( 'X') &&  index < 9 ){ 
       
            s +=10 ; 
-           s += this.strikeBouns(frames,index);   
- 
-             
           
+          // console.log('index',index , frames[index +1].includes('X'));
+      
+
+          if(typeof frames[index+1] !== 'undefined' && frames[index+1].includes('X')){
+             s+=10;
+              if(typeof frames[index+2] !== 'undefined'){
+                if(frames[index+1].includes('X') && frames[index+2].includes('X')){
+                  s+=10;
+                }else{
+                   s+= this.format(frames[index+2][0]);
+                }
+              }
+           }else{
+          s += frames[index+1].reduce((a,b)=>this.format(a)+this.format(b) , 0);
+        }
+      
+        
+        
+
+
           //Add Bouns
       }
       else if(frame.includes( '/')){ 
           s += 10 + this.spareBouns(frames,index);
       }
       else{
-        s += frame.reduce((a,b)=>a+b , 0);
+        s += frame.reduce((a,b)=>this.format(a)+this.format(b) , 0);
       }
       return s;
     },
