550d0b1479967ea84becc42334c6ee2e4b34ebcf
Nomi <naumanahmed19@gmail.com>
2021-01-30 19:38:56 +0100

    enh(game): score calculation refactor

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@
 
 <script>
 import ScoreBoard from './components/ScoreBoard.vue'
-  var strike =0;
 export default {
   name: 'App',
   components: {
@@ -39,19 +38,17 @@ export default {
       players: [
       {
         name: 'Ahmad',
-    //frames: [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
-      frames:[[8,'/'],[9,0],[4,4],[7,2],[9,0],['X'],['X'],[8,0],[3,5],[9,'/',7]],
-     //frames: [['X'],['X'],['X'],[5,1],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
+        frames: [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
         faramScore:[],
       },
       {
 
         name: 'Ali',
-       frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],[8,0],[5,1],[3,'/'],[9,0,0]],
-        //  frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],['X'],[8,0],[3,'/'],[9,0,0]],
+        frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],[8,0],[5,1],[3,'/'],[9,0,0]],
         faramScore:[],
         
-      }
+      },
+      
     ],
     }
   },
@@ -80,49 +77,34 @@ export default {
       
       return score;
     },
-    hasFrame(index){
-      return typeof frames[index] !== 'undefined'
+    hasFrame(frames, index){
+      return typeof frames[index] !== 'undefined' && frames[index].includes('X');
     },
     calculeFrameScore(frames,frame,index){
-     
       let s = 0;
-     // if(index == 10) return s;  //TODO: Check 
-     
       if(frame.includes( 'X') && index == 8 ){
-
-          s +=frames[index+1].reduce((a,b)=>this.format(a)+this.format(b) , 0) ; 
-
+          s += this.sum(frames[index+1]); 
       }
-     else if(frame.includes( 'X') &&  index < 9 ){ 
-      
-           s +=10 ; 
-          
-          // console.log('index',index , frames[index +1].includes('X'));
-      
-
-          if(typeof frames[index+1] !== 'undefined' && frames[index+1].includes('X')){
-             s+=10;
-              if(typeof frames[index+2] !== 'undefined'){
-                if(frames[index+1].includes('X') && frames[index+2].includes('X')){
-                  s+=10;
-                }else{
-                   s+= this.format(frames[index+2][0]);
-                }
-              }
-           }else{
-          s += frames[index+1].reduce((a,b)=>this.format(a)+this.format(b) , 0);
-        }
-      
-        
-        
-
-
-          //Add Bouns
+      else if(frame.includes( 'X') &&  index < 9 ){ 
+          s += 10 + this.strikeBouns(frames,index); 
       } 
       else if(frame.includes( '/') && index < 9){ 
           s += 10 + this.spareBouns(frames,index);
       }
       else{
+        s += this.caulculateLastFrame(frame);
+      }
+      return s;
+    },
+
+    sum(frame){
+        return frame.reduce((a,b)=>this.format(a)+this.format(b) , 0);
+    },
+
+    
+
+    caulculateLastFrame(frame){
+      let s = 0;
         /**
          * if last frame has spare 
          */
@@ -130,26 +112,35 @@ export default {
            let spareIndex = frame.indexOf('/');
            let f = frame.filter(s=>(s !=='/'));
            s+= 10- frame[spareIndex -1]; 
-           s += f.reduce((a,b)=>this.format(a)+this.format(b) , 0);
+           s += this.sum(f);
         }else{
-            s += frame.reduce((a,b)=>this.format(a)+this.format(b) , 0);
+            s += this.sum(frame);
         } 
-       
-      }
-      return s;
+      return s;  
     },
-
-
-    
     
     spareBouns(frames,index){
         console.log(frames[index+1][0]);
       return this.format(frames[index+1][0]);
     },
 
-    strikeBouns(frames,index){
+     strikeBouns(frames,index){
+        let s = 0;
+       if(this.hasFrame(frames, index+1)){
+             s+=10;
+              if(typeof frames[index+2] !== 'undefined'){
+                if(this.hasFrame(frames, index+1) && this.hasFrame(frames, index+2)){
+                  s+=10;
+                }else{
+                   s+= this.format(frames[index+2][0]);
+                }
+              }
+           }else{
+             s += this.sum(frames[index+1]);
+          }
+          return s;
  
-      return this.calculeFrameScore(frames,frames[index+1], index+1);
+     // return this.calculeFrameScore(frames,frames[index+1], index+1);
      
     },
 
