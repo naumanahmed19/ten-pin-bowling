e6d0f86c894816c9f1f00a2e7ecf4ff89f47d655
Nomi <naumanahmed19@gmail.com>
2021-01-30 22:52:31 +0100

    enh(player): option to add new players

--- src/components/Game/Game.vue
+++ src/components/Game/Game.vue
@@ -1,6 +1,28 @@
 <template>
   <ScoreBoard />
-  <h3>Current Turn : {{players[currentPlayer].name}}</h3>
+  
+  <button @click="handleStartNewGame">Start New Game</button>
+  <button @click="handleResetGame">Replay Game</button>
+  <hr>
+
+  <div v-if="!isGameActive">
+      Add Players:
+  <input type="text" v-model="playerName" >
+  <button @click="handleAddPlayer">Add Player</button>
+  </div>
+
+
+  <div v-if="!isGameActive && players.length">
+  <ul>
+      <li v-for="(player,index) in players" :key="index">
+        {{player.name}} 
+      </li>
+  </ul>
+  <button @click="handleStartGame">Start Game</button>
+  </div>
+
+  <div v-if="isGameActive &&  players.length">
+      <h3>Current Turn : {{players[currentPlayer].name}}</h3>
   <button @click="changeTurn(1)">Change Turn</button>
     <button @click="handleRestGame">Reset Game</button>
   <ul>
@@ -22,6 +44,8 @@
         </table>
       </li>
   </ul>
+   
+  </div>
 </template>
 
 <script>
@@ -34,28 +58,62 @@ export default {
   },
   data() {
     return {
+      isGameActive: false,
+      playerName: '',
       currentPlayer : 0,
       previousValue: 0,
   
       players: [
-      {
-        name: 'Ahmad',
-        frames: [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
-        faramScore:[],
-      },
-      {
+      // {
+      //   name: 'Ahmad',
+      //   frames: [['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X'],['X','X','X']],
+      //   faramScore:[],
+      // },
+      // {
 
-        name: 'Ali',
-        frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],[8,0],[5,1],[3,'/'],[9,0,0]],
-        faramScore:[],
+      //   name: 'Ali',
+      //   frames:[[8,0],[7,0],[5,3],[9,'/'],[9,'/'],['X'],[8,0],[5,1],[3,'/'],[9,0,0]],
+      //   faramScore:[],
         
-      },
-      
+      // },
+
     ],
     }
   },
 
   methods: {
+   
+   /**
+     * Starting a game
+     */
+    handleStartGame(){
+      this.isGameActive = true;
+    },
+
+    /**
+     * Starting a new game
+     * Reset all players data
+     */
+    handleNewStartGame(){
+      this.isGameActive = true;
+      this.players = [];
+    },
+
+    /**
+     * Add New Player
+     */
+    handleAddPlayer(){
+      if(this.playerName){
+        this.players.push({ 
+        name: this.playerName,
+        frames:[],
+        faramScore:[],
+      });
+      }
+    
+      //reset field
+      this.playerName ='';
+    },
 
     handleRestGame(){
       this.players.forEach((fram,index)=>{
