0b0724801ce9bc0bee34a7ecb5cfd12f23041cbc
Nomi <naumanahmed19@gmail.com>
2021-01-31 22:29:35 +0100

    enh(ui): element-ui added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css";
 import './index.css'
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+
+createApp(App).use(ElementPlus).mount("#app");
