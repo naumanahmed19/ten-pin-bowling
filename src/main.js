933a0dd322d63f5ec57f367cc2f0ed446e2fe9c7
Nomi <naumanahmed19@gmail.com>
2021-01-31 22:47:08 +0100

    fix(ui): removed elemnt-ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-
-import { createApp } from "vue";
-import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
+import { createApp } from 'vue'
+import App from './App.vue'
 import './index.css'
 
-import App from "./App.vue";
-
-createApp(App).use(ElementPlus).mount("#app");
+createApp(App).mount('#app')
