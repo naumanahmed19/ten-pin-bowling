f220f3c21669632695d5ddc7a89cadd4fbf476db
Nomi <naumanahmed19@gmail.com>
2021-02-01 05:10:40 +0100

    enh(common): Alert component added

---
+++ src/components/Common/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-indigo-400"
+  >
+    <span class="inline-block align-middle mr-8">
+      <b class="capitalize">{{ title }}: </b>
+      {{ message }}
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
